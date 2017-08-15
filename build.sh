9288d818d10fe3f7f7990f1cc20179cee01493f8
alixian <xianguo@maihaoche.com>
2017-08-15 21:47:35 +0800

    第一次提交

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+SPRING_PROFILES_ACTIVE=dev
+PROJECT_NAME=moyu
+PROJECT_HOME=/home/admin/app/
+JAR_DIR=${PROJECT_NAME}/target
+JAR_NAME=${PROJECT_NAME}.jar
+
+cd ${PROJECT_HOME}
+ls
+mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
+cd ${JAR_DIR}
+EUREKA_PID=`ps -ef | grep "eureka" | grep -v grep | awk '{print $2}'`
+echo "Eureka PID= $EUREKA_PID"
+#kill -9 MOYU_PID
+java -jar ${JAR_NAME} --spring.profiles.active=${SPRING_PROFILES_ACTIVE}
+echo "========Build successful======="
