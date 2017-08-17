5228674c11c559c0a6872b0fc17a127c534e757f
alixian <xianguo@maihaoche.com>
2017-08-17 18:46:47 +0800

    dada

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ SPRING_PROFILES_ACTIVE=dev
 PROJECT_HOME=/home/admin/app/
 JAR_DIR=${PROJECT_HOME}target
 JAR_NAME=${PROJECT_NAME}.jar
-cd PROJECT_HOME
+cd ${PROJECT_HOME}
 ls
 mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
 cd ${JAR_DIR}
