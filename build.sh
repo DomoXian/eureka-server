f2268526b0b7ccb12bf78e75680dd461d10a6904
李贤 <xianguo@maihaoche.com>
2017-09-26 09:41:53 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 PROJECT_NAME=eureka-server
 PROJECT_HOME=/home/admin/app/
 JAR_DIR=${PROJECT_HOME}target
-JAR_NAME=${JAR_NAME}.jar
+JAR_NAME=${PROJECT_NAME}.jar
 cd ${PROJECT_HOME}
 ls
 mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
