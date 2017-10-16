5793af704c5237cbd2b7788d2399212943379de2
李贤 <xianguo@maihaoche.com>
2017-10-16 15:33:26 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-PROJECT_NAME=eureka-server
 PROJECT_HOME=/home/admin/app/
-JAR_DIR=${PROJECT_HOME}target
+JAR_DIR=${PROJECT_NAME}-core/target
 JAR_NAME=${PROJECT_NAME}.jar
 cd ${PROJECT_HOME}
 ls
 mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
 cd ${JAR_DIR}
-EUREKA_PID=`ps -ef | grep "eureka" | grep -v grep | awk '{print $2}'`
-echo "Eureka PID= $EUREKA_PID"
-#kill -9 MOYU_PID
-java -jar ${JAR_NAME} --spring.profiles.active=${SPRING_PROFILES_ACTIVE}
+DOMO_PID=`ps -ef | grep "DOMO" | grep -v grep | awk '{print $2}'`
+echo "DOMO PID = $DOMO_PID"
+#kill -9 DOMO_PID
+java -jar ${JAR_NAME} --spring.profiles.active=${ENV}
 echo "========Build successful======="
