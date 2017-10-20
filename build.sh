9ff2a232e1bee48ef622a8c83bbcef6ef53db02e
李贤 <xianguo@maihaoche.com>
2017-10-20 17:09:39 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,5 @@ cd ${PROJECT_HOME}
 ls
 mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
 cd ${JAR_DIR}
-DOMO_PID=`ps -ef | grep "DOMO" | grep -v grep | awk '{print $2}'`
-echo "DOMO PID = $DOMO_PID"
-#kill -9 DOMO_PID
 java -jar ${JAR_NAME} --spring.profiles.active=${ENV}
 echo "========Build successful======="
